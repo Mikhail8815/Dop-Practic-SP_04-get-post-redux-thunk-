58d29a4d711e9559a7d07a6b8439c2f1457dc957
Mikhail8815 <mishutka150588@mail.ru>
2025-04-05 18:24:22 +0300

    Redux Flow

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,12 +1,34 @@
 import s from './DecksList.module.css'
-import {useEffect} from "react";
-import {decksAPI} from "../decks-api.ts";
+import { useEffect, useState } from 'react'
+import { Deck, decksAPI, GetDecksResponse } from '../decks-api.ts'
+import { setDecksAC } from '../decks-reducer.ts'
+import { AppRootState, useAppDispatch, useAppSelector } from '../../../app/store.ts'
+import { DeckItem } from './DeckItem/DeckItem.tsx'
+import { selectDecks } from '../decks-selectors.ts'
 
 export const DecksList = () => {
+  // const [decks, setDecks] = useState<Deck[]>([])
+  const dispatch = useAppDispatch()
+
+  const decks = useAppSelector(selectDecks)
+
   useEffect(() => {
-    decksAPI.fetchDecks().then((res) => {
-      console.log(res.data)
+    // const fetchDecks = async () => {
+    //   try {
+    //     const response = await decksAPI.fetchDecks()
+    //     dispatch(setDecksAC(response.data.items)) // Достаём колоды из response.data.items
+    //   } catch (error) {
+    //     console.error('Error fetching decks:', error)
+    //   }
+    // }
+    // fetchDecks()
+    decksAPI.fetchDecks().then((res)=>{
+      dispatch(setDecksAC(res.data.items))
     })
   }, []);
-  return <ul className={s.list}></ul>
+  return <ul className={s.list}>
+    {decks.map((deck) => (
+      <DeckItem key={deck.id} deck={deck}/>
+    ))}
+  </ul>
 }
--- src/features/decks/decks-reducer.ts
+++ src/features/decks/decks-reducer.ts
@@ -1,5 +1,8 @@
+import { Deck } from './decks-api.ts'
+import { Dispatch } from 'react'
+
 const initialState = {
-  decks: [] as any[], // todo: add type
+  decks: [] as Deck[], // todo: add type
   searchParams: {
     name: '',
   },
@@ -9,9 +12,29 @@ type DecksState = typeof initialState
 
 export const decksReducer = (state: DecksState = initialState, action: DecksActions): DecksState => {
   switch (action.type) {
+    case 'SET_DECKS':
+      return {...state, decks: action.payload.decks}
     default:
       return state
   }
 }
 
-type DecksActions = any
+// type SetDecksActionType = {
+//   type: 'SET_DECKS'
+//   payload: {
+//     decks: Deck[]
+//   }
+// }
+
+type  SetDecksActionType = ReturnType<typeof setDecksAC>
+export type DecksActions = SetDecksActionType
+
+
+export const setDecksAC = (decks: Deck[]) => {
+  return {
+    type: 'SET_DECKS',
+    payload: {
+      decks: decks
+    }
+  }
+}
